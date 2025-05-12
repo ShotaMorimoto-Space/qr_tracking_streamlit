37036f3cc7c066c0268e188f49af34291c966777
ShotaMorimoto-Space <s.morimoto1101@gmail.com>
2025-05-12 15:00:53 +0900

    db change

--- backend/db_control/database.py
+++ backend/db_control/database.py
@@ -2,19 +2,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # SQLite接続パス（tracking_app_dbフォルダ内）
 #DATABASE_URL = "sqlite:///tracking_app_db/access_logs.db"
 
 # Renderでは /tmp を使う
-DB_PATH = "/tmp/access_logs.db"
-DATABASE_URL = f"sqlite:///{DB_PATH}"
+#DB_PATH = "/tmp/access_logs.db"
+#DATABASE_URL = f"sqlite:///{DB_PATH}"
 
-# エンジン作成
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}  # SQLiteの場合はこれ必須
-)
+# PostgreSQL用の接続文字列（Renderの情報に置き換えてください）
+DATABASE_URL = "postgresql://<USER>:<PASSWORD>@<HOST>:<PORT>/<DB_NAME>"
+
+engine = create_engine(DATABASE_URL)
 
 # セッション作成
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

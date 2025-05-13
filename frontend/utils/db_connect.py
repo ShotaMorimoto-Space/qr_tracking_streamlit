26124f54b3902462d5702825fad8fe8e84ae5a31
ShotaMorimoto-Space <s.morimoto1101@gmail.com>
2025-05-13 13:38:06 +0900

    hardcode

--- frontend/utils/db_connect.py
+++ frontend/utils/db_connect.py
@@ -10,7 +10,7 @@ def get_streamlit_db():
     db_port = int(str(st.secrets["database"]["port"]).strip())
     db_name = st.secrets["database"]["database"]
 
-    db_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+    db_url = f"postgresql://access_logs_db_user:DWAXFSpfyAxkEBu5HCs624F2fyHeKbFQ@dpg-d0gontjuibrs73fqm64g-a.singapore-postgres.render.com/access_logs_db"
     engine = create_engine(db_url)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     return SessionLocal()
